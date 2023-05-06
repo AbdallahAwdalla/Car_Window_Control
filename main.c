f37906e2e7f8f6303e824b93065b610f19061488
Abdallah Awdallah <awdallah52@gmail.com>
2023-05-07 02:51:33 +0300

    CWC-001: Blinky Example

--- main.c
+++ main.c
@@ -1,155 +1,92 @@
-/*
- * FreeRTOS Kernel V10.0.0
- * Copyright (C) 2017 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- * the Software, and to permit persons to whom the Software is furnished to do so,
- * subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software. If you wish to use our Amazon
- * FreeRTOS name, please do so in a fair use way that does not cause confusion.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- * http://www.FreeRTOS.org
- * http://aws.amazon.com/freertos
- *
- * 1 tab == 4 spaces!
- */
- 
-
-/******************************************************************************
- *  MODIFICATIONS:
- *  03/19/2018 - Modified to run on the EK-TM4C123GXL development board. jspicer
- */
- 
-/******************************************************************************
- *
- * main creates one queue, one software timer, and two tasks.  It then
- * starts the scheduler.
- *
- * The Queue Send Task:
- * The queue send task is implemented by the prvQueueSendTask() function in
- * this file.  It uses vTaskDelayUntil() to create a periodic task that sends
- * the value 100 to the queue every 200 milliseconds (please read the notes
- * above regarding the accuracy of timing under Windows).
- *
- * The Queue Send Software Timer:
- * The timer is a one-shot timer that is reset by a SW1 press.  The timer's
- * period is set to two seconds - if the timer expires then its callback
- * function writes the value 200 to the queue.  The callback function is
- * implemented by prvQueueSendTimerCallback() within this file.
- *
- * The Queue Receive Task:
- * The queue receive task is implemented by the Task01() function
- * in this file.  Task01() waits for data to arrive on the queue.
- * When data is received, the task checks the value of the data, then blinks
- * the green LED to indicate that the data came from the queue send task and
- * the red LED to indicate that it came from the queue send software timer.
- *
- * Expected Behaviour:
- * - The queue send task writes to the queue every 200ms, so every 200ms the
- *   queue receive task will blink the green LED indicating that data was received
- *   on the queue from the queue send task.
- * - The queue send software timer has a period of two seconds, and is reset
- *   each time SW1 is pressed.  So if two seconds expire without SW1 being
- *   pressed then the queue receive task will blink the red LED indicating that
- *   data was received on the queue from the queue send software timer.
- *
- */
-
-/* Kernel includes. */
+/**
+ ********************************************************************************
+ * @file    main
+ * @author  Abdallah and 7oda 
+ * @date    
+ * @brief    Here is where the main function live :) 
+ ********************************************************************************
+*/
+/********************************************************************************
+ * INCLUDES
+ ********************************************************************************/
+#include <stdint.h>
+#include <stdbool.h>
 #include <FreeRTOS.h>
 #include <task.h>
 #include <semphr.h>
 #include <timers.h>
+#include "inc/hw_memmap.h"
+#include "driverlib/debug.h"
+#include "driverlib/gpio.h"
+#include "driverlib/sysctl.h"
 
-/* Hardware includes */
+
+/********************************************************************************
+ * PRIVATE MACROS AND DEFINES
+ ********************************************************************************/
+/// \brief Priorities at which the tasks are created
+#define TASK_01_PRIORITY		( tskIDLE_PRIORITY + 2 )
 
 
-/* Priorities at which the tasks are created. */
-#define mainQUEUE_RECEIVE_TASK_PRIORITY		( tskIDLE_PRIORITY + 2 )
-#define	mainQUEUE_SEND_TASK_PRIORITY		( tskIDLE_PRIORITY + 1 )
+/********************************************************************************
+ * PRIVATE TYPEDEFS
+ ********************************************************************************/
 
-/* The rate at which data is sent to the queue.  The times are converted from
-milliseconds to ticks using the pdMS_TO_TICKS() macro. */
-#define mainTASK_SEND_FREQUENCY_MS			pdMS_TO_TICKS( 200UL )
-#define mainTIMER_SEND_FREQUENCY_MS			pdMS_TO_TICKS( 2000UL )
+/********************************************************************************
+ * STATIC VARIABLES
+ ********************************************************************************/
+static uint8_t myFlag;
+/********************************************************************************
+ * GLOBAL VARIABLES
+ ********************************************************************************/
 
-/* These are ON durations for the blinking LEDS */
-#define mainTASK_BLINK_DELAY_MS					pdMS_TO_TICKS( 20UL )
-#define mainTIMER_BLINK_DELAY_MS				pdMS_TO_TICKS( 200UL )
 
-/* The number of items the queue can hold at once. */
-#define mainQUEUE_LENGTH					( 2 )
 
-/* The values sent to the queue receive task from the queue send task and the
-queue send software timer respectively. */
-#define mainVALUE_SENT_FROM_TASK			( 100UL )
-#define mainVALUE_SENT_FROM_TIMER			( 200UL )
 
-/*-----------------------------------------------------------*/
+/********************************************************************************
+ * STATIC FUNCTION PROTOTYPES
+ ********************************************************************************/
 
-/*
- * The tasks as described in the comments at the top of this file.
- */
 static void Task01( void *pvParameters );
+static void MyBlinkyLED_Init( void );
 
-/*-----------------------------------------------------------*/
-
-/* The queue used by both tasks. */
-static QueueHandle_t xQueue = NULL;
-
-/* A software timer that is started from the tick hook. */
-static TimerHandle_t xTimer = NULL;
-
-/*-----------------------------------------------------------*/
-
-
-
-/* These are the bit-band addresses for the onboard LEDs  */
-volatile uint32_t* redLED_;
-volatile uint32_t* greenLED_;
-
-
-
-
-
-
-/*** SEE THE COMMENTS AT THE TOP OF THIS FILE ***/
+/********************************************************************************
+ * FUNCTION NAME:       main
+ * \param  [in]         void
+ * \param  [out]        int
+ * 
+ * 
+********************************************************************************/
 int main( void )
 {
-		/* Start the two tasks as described in the comments at the top of this file. */
-		xTaskCreate( Task01,			/* The function that implements the task. */
-					"Rx", 													/* The text name assigned to the task - for debug only as it is not used by the kernel. */
-					configMINIMAL_STACK_SIZE, 			/* The size of the stack to allocate to the task. */
-					NULL, 													/* The parameter passed to the task - not used in this simple case. */
-					mainQUEUE_RECEIVE_TASK_PRIORITY,/* The priority assigned to the task. */
-					NULL );													/* The task handle is not required, so NULL is passed. */
+    MyBlinkyLED_Init();
+
+    /* Start the two tasks as described in the comments at the top of this file. */
+    xTaskCreate( Task01,			        /* The function that implements the task. */
+                 "Task01", 					/* The text name assigned to the task - for debug only as it is not used by the kernel. */
+                 configMINIMAL_STACK_SIZE, 	/* The size of the stack to allocate to the task. */
+                 NULL, 						/* The parameter passed to the task - not used in this simple case. */
+                 TASK_01_PRIORITY,          /* The priority assigned to the task. */
+                 NULL );					/* The task handle is not required, so NULL is passed. */
 
 
-		vTaskStartScheduler();
+    vTaskStartScheduler();
 
-	/* If all is well, the scheduler will now be running, and the following
-	line will never be reached.  If the following line does execute, then
-	there was insufficient FreeRTOS heap memory available for the idle and/or
-	timer tasks	to be created.  See the memory management section on the
-	FreeRTOS web site for more details. */
-	for( ;; );
+    /* If all is well, the scheduler will now be running, and the following
+    line will never be reached.  If the following line does execute, then
+    there was insufficient FreeRTOS heap memory available for the idle and/or
+    timer tasks	to be created.  See the memory management section on the
+    FreeRTOS web site for more details. */
+    for( ;; );
 }
-/*-----------------------------------------------------------*/
 
-
-
+/********************************************************************************
+ * FUNCTION NAME:       Task01
+ * \param  [in]         void *pvParameters
+ * \param  [out]        void 
+ * 
+ * 
+********************************************************************************/
 static void Task01( void *pvParameters )
 {
 	uint32_t ulReceivedValue;
@@ -159,6 +96,43 @@ static void Task01( void *pvParameters )
 
 	for( ;; )
 	{
+        if (0U ==  myFlag)
+        {
+            /* Turn on the LED.*/
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, GPIO_PIN_3);
+        }
+        else
+        {
+            /* Turn on the LED.*/
+            GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, 0x0u);
+        }
+        myFlag = (myFlag + 1U) % 2U;
+        
 		vTaskDelay(1000);
 	}
+}
+
+
+/********************************************************************************
+ * FUNCTION NAME:       MyBlinkyLED_Init
+ * \param  [in]         void *pvParameters
+ * \param  [out]        void 
+ * 
+ * 
+********************************************************************************/
+static void MyBlinkyLED_Init( void )
+{
+    
+    /* Enable the GPIO port that is used for the on-board LED. */  
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+
+    /* Check if the peripheral access is enabled. */ 
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF))
+    {
+    }
+
+    /* Enable the GPIO pin for the LED (PF3).  Set the direction as output, and */
+    /* enable the GPIO pin for digital function. */
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_3);
+
 }
